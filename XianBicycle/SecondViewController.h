be4c5b6cac44ddf83b3da29c6a1e81aebbbe7750
Zhang Shuai <zhang.shuai@rea-group.com>
2015-07-11 14:24:28 +0800

    Initial Commit

---
+++ XianBicycle/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  XianBicycle
+//
+//  Created by Shuai Zhang on 7/11/15.
+//  Copyright (c) 2015 ZhangShuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ XianBicycle/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  XianBicycle
+//
+//  Created by Shuai Zhang on 7/11/15.
+//  Copyright (c) 2015 ZhangShuai. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
