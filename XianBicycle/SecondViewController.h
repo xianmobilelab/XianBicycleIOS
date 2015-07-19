173da11fb3890cf508d18a29e6c02f490d7e7592
Zhang Shuai <shuaiz@thoughtworks.com>
2015-07-20 00:01:29 +0800

    Add BaiduMap BMKSuggestionSearch for SecondViewController.

--- XianBicycle/SecondViewController.h
+++ XianBicycle/SecondViewController.h
@@ -9,7 +9,23 @@
 #import <UIKit/UIKit.h>
 #import <BaiduMapAPI/BMapKit.h>
 
-@interface SecondViewController : UIViewController
+@interface BicycleSuggestionResult : NSObject
+
+///key列表，成员是NSString
+@property (nonatomic, strong) NSMutableArray* keyList;
+///district列表，成员是NSString
+@property (nonatomic, strong) NSMutableArray* districtList;
+///pt列表，成员是：封装成NSValue的CLLocationCoordinate2D
+@property (nonatomic, strong) NSMutableArray* ptList;
+
+-(id)initWithCapacity:(NSUInteger) capacity;
+
+@end
+
+@interface SecondViewController : UIViewController <BMKSuggestionSearchDelegate> {
+    BMKSuggestionSearch *_searcher;
+    BicycleSuggestionResult *_suggestionResult;
+}
 
 @property (weak, nonatomic) IBOutlet UITextField *endTextField;
 @property (weak, nonatomic) IBOutlet UITextField *startTextField;
--- XianBicycle/SecondViewController.m
+++ XianBicycle/SecondViewController.m
@@ -8,7 +8,26 @@
 
 #import "SecondViewController.h"
 
+
 @interface SecondViewController ()
+
+@end
+
+@implementation BicycleSuggestionResult
+
+@synthesize keyList;
+@synthesize districtList;
+@synthesize ptList;
+
+-(id)initWithCapacity:(NSUInteger) capacity
+{
+    if (self = [super init]) {
+        self.keyList = [[NSMutableArray alloc] initWithCapacity:capacity];
+        self.districtList = [[NSMutableArray alloc] initWithCapacity:capacity];
+        self.ptList = [[NSMutableArray alloc] initWithCapacity:capacity];
+    }
+    return self;
+}
 
 @end
 
@@ -16,6 +35,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _searcher = [[BMKSuggestionSearch alloc] init];
+    _searcher.delegate = self;
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.title = NSLocalizedString(@"tab_search", nil);
 }
@@ -25,10 +46,66 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)doSearch:(id)sender {
-    NSLog(@"start :%@", _startTextField.text);
-    NSLog(@"start :%@", _endTextField.text);
+- (void)viewWillDisappear:(BOOL)animated
+{
+    _searcher.delegate = nil;
 }
 
+- (IBAction)doSearch:(id)sender {
+    NSLog(@"start :%@", _startTextField.text);
+    NSLog(@"end :%@", _endTextField.text);
+    [self searchByKeyWord:_startTextField.text];
+}
+
+#pragma mark - private methods
+- (void)searchByKeyWord:(NSString *)keyword
+{
+    BMKSuggestionSearchOption *option = [[BMKSuggestionSearchOption alloc] init];
+    option.cityname = @"西安市";
+    option.keyword = keyword;
+    BOOL flag = [_searcher suggestionSearch:option];
+    if(flag)
+    {
+        NSLog(@"建议检索发送成功");
+    }
+    else
+    {
+        NSLog(@"建议检索发送失败");
+    }
+}
+
+- (void)displaySuggestionResult
+{
+    for(int i = 0; i < [_suggestionResult.keyList count];i++) {
+        CLLocationCoordinate2D coordinate;
+        [[_suggestionResult.ptList objectAtIndex: i]getValue:&coordinate];
+        NSLog(@"%@ - %@, %f, %f", _suggestionResult.keyList[i], _suggestionResult.districtList[i], coordinate.latitude, coordinate.longitude);
+    }
+}
+
+#pragma mark - BMKSuggestionSearchDelegate
+- (void)onGetSuggestionResult:(BMKSuggestionSearch*)searcher result:(BMKSuggestionResult*)result errorCode:(BMKSearchErrorCode)error
+{
+    if (error == BMK_SEARCH_NO_ERROR) {
+        _suggestionResult = nil;
+        _suggestionResult = [[BicycleSuggestionResult alloc] initWithCapacity:[result.keyList count]];
+        for (int i = 0; i < [result.keyList count]; i++) {
+            if ([result.cityList[i] isEqualToString:@"西安市"]) {
+                [_suggestionResult.keyList addObject:result.keyList[i]];
+                [_suggestionResult.districtList addObject:result.districtList[i]];
+                [_suggestionResult.ptList addObject:result.ptList[i]];
+            }
+        }
+        [self displaySuggestionResult];
+    } else {
+        NSLog(@"抱歉，未找到结果");
+        if (_suggestionResult == nil) {
+            _suggestionResult = [[BicycleSuggestionResult alloc] initWithCapacity:0];
+        }
+        [_suggestionResult.keyList removeAllObjects];
+        [_suggestionResult.districtList removeAllObjects];
+        [_suggestionResult.ptList removeAllObjects];
+    }
+}
 
 @end
